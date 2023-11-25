ebe9a2eb066f99d2bb1cefb6342b1a416d8dba78
stefania281 <118693590+stefania281@users.noreply.github.com>
2023-11-25 15:05:09 +0200

    Car Project

---
+++ car project1/car project1/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace car_project1
+{
+    public class Car
+    {
+        public string color;
+        public string name;
+        public int maxSpeed;
+        public int year;
+        public Car(string colorName, string nameName, int maxSpeedValue, int yearValue)
+        {
+            name= nameName;
+            maxSpeed = maxSpeedValue;
+            year = yearValue;
+            color= colorName;
+
+        }
+        public Car(string nameName)
+        {
+            name = nameName;
+        }
+        public void fullThrottle()
+        {
+            Console.WriteLine(name + "is going at the speed of " + maxSpeed);
+        }
+        
+
+    }
+}
---
+++ car project1/car project1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace car_project1
+{
+    internal class Program
+    {
+        string color = "red";
+        static void Main(string[] args) {
+
+            Car Ford= new Car("Mustang", "white", 2021,100);
+            Car Opel = new Car("Astra", "blue", 2020, 100);
+            Car Volvo = new Car("XC-90");
+            Ford.name="Mustang";
+            Ford.year=2021;
+            Ford.color="white";
+            Ford.maxSpeed=100;
+            Ford.fullThrottle();
+            Console.WriteLine(Ford.name);
+            Console.WriteLine(Ford.year);
+            Console.WriteLine(Ford.color);
+            Opel.name="Astra";
+            Opel.year=2020;
+            Opel.color= "blue";
+            Opel.maxSpeed=120;
+            Opel.fullThrottle();
+            Console.WriteLine(Opel.name);
+            Console.WriteLine(Opel.year);
+            Console.WriteLine(Opel.color);
+
+            Console.ReadKey();
+       
+
+        }
+
+       
+
+    }
+}
